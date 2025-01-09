a673c2139395b4cd2b993fb204c832165dc5dcdd
lucas-laza <laza.lucas15@gmail.com>
2025-01-09 11:10:58 +0100

    lighthouse js

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,5 +1,9 @@
 module.exports = {
   ci: {
+    collect: {
+      staticDistDir: './web', // Adjust this path to your build directory
+      // Other configurations
+    },
     upload: {
       target: 'temporary-public-storage',
     },
