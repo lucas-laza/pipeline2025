40a27dfc4fe70c28ca3bd2585e7a5a1113fee75c
lucas-laza <lucas-laza@users.noreply.github.com>
2025-01-10 13:50:08 +0000

    deploy: 9e7f975ad16aa7be5ff4768a2389f49ef27100c3

---
+++ code/js/searchIndex.js
@@ -0,0 +1,34 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\Drupal\\custom_test_coucou\\Controller\\CustomTestCoucouController",
+            "name": "CustomTestCoucouController",
+            "summary": "Contr\u00F4leur\u0020pour\u0020le\u0020module\u0020custom_test_coucou.",
+            "url": "classes/Drupal-custom-test-coucou-Controller-CustomTestCoucouController.html"
+        },                {
+            "fqsen": "\\Drupal\\custom_test_coucou\\Controller\\CustomTestCoucouController\u003A\u003AdisplayMessage\u0028\u0029",
+            "name": "displayMessage",
+            "summary": "Retourne\u0020une\u0020page\u0020avec\u0020le\u0020message\u0020\u0022Coucou\u0020ceci\u0020est\u0020un\u0020test\u0022.",
+            "url": "classes/Drupal-custom-test-coucou-Controller-CustomTestCoucouController.html#method_displayMessage"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        },                {
+            "fqsen": "\\Drupal\\custom_test_coucou\\Controller",
+            "name": "Controller",
+            "summary": "",
+            "url": "namespaces/drupal-custom-test-coucou-controller.html"
+        },                {
+            "fqsen": "\\Drupal\\custom_test_coucou",
+            "name": "custom_test_coucou",
+            "summary": "",
+            "url": "namespaces/drupal-custom-test-coucou.html"
+        },                {
+            "fqsen": "\\Drupal",
+            "name": "Drupal",
+            "summary": "",
+            "url": "namespaces/drupal.html"
+        }            ]
+);
