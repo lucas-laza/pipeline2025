927a677acf35b22c40c6bab9c9027d68bbed2e70
lucas-laza <laza.lucas15@gmail.com>
2025-01-10 14:35:52 +0100

    module

---
+++ web/modules/custom/custom_test_coucou/src/Controller/CustomTestCoucouController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Drupal\custom_test_coucou\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Contrôleur pour le module custom_test_coucou.
+ */
+class CustomTestCoucouController extends ControllerBase {
+
+  /**
+   * Retourne une page avec le message "Coucou ceci est un test".
+   *
+   * @return array
+   *   Un rendu pour une page de contenu.
+   */
+  public function displayMessage() {
+    return [
+      '#markup' => $this->t('Coucou ceci est un test.'),
+    ];
+  }
+}
