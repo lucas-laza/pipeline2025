cdde92d07828333377cb3deeb9fb5402c931fdc1
lucas-laza <lucas-laza@users.noreply.github.com>
2025-01-10 14:45:10 +0000

    deploy: 6717e2c683ff522138b2da62ee07be2ab60cd850

--- code/js/searchIndex.js
+++ code/js/searchIndex.js
@@ -11,6 +11,11 @@ Search.appendIndex(
             "summary": "Retourne\u0020une\u0020page\u0020avec\u0020le\u0020message\u0020\u0022Coucou\u0020ceci\u0020est\u0020un\u0020test\u0022.",
             "url": "classes/Drupal-custom-test-coucou-Controller-CustomTestCoucouController.html#method_displayMessage"
         },                {
+            "fqsen": "\\Drupal\\custom_test_coucou\\Controller\\CustomTestCoucouController\u003A\u003AuselessFunction\u0028\u0029",
+            "name": "uselessFunction",
+            "summary": "Cette\u0020fonction\u0020ne\u0020sert\u0020\u00E0\u0020rien",
+            "url": "classes/Drupal-custom-test-coucou-Controller-CustomTestCoucouController.html#method_uselessFunction"
+        },                {
             "fqsen": "\\",
             "name": "\\",
             "summary": "",
